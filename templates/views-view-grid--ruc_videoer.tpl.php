b41af0fd838aab20e1bf0b59f9ecd4da834c860c
Lars Olesen <lars@intraface.dk>
2012-05-31 12:43:12 +0200

    Now colorbox and videoes has been implemented

--- templates/views-view-grid--ruc_videoer.tpl.php
+++ templates/views-view-grid--ruc_videoer.tpl.php
@@ -5,6 +5,9 @@
     <div class="inner">
     <h2>Tester</h2>
     <p>Min relativt lange tekst, som bare fortsætter indtil pladsen er løbet helt ud. Det aner vi ikke hvornår det er, men mon ikke det er snart? Hm, det er slet ikke endnu. Der er endnu plads nok. Så skriv bare hertil.</p>
+    <?php if ($is_front): ?>
+    <?php echo l('Se alle videoer...', 'videoer'); ?>
+    <?php endif; ?>
     </div>
 </div>
 <?php
