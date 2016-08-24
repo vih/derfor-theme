edcfcbc449b7fdccda992271d5675543c4da6be3
Lars Olesen <lars@intraface.dk>
2016-08-24 06:39:26 +0000

    Added Libsass integration for easier changes

---
+++ Gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sassdoc = require('sassdoc');
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+var sassdocOptions = {
+    dest: './public/sassdoc'
+};
+
+var input = './sass/**/*.scss';
+var output = './css';
+
+gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        .pipe(sourcemaps.init())
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('sassdoc', function () {
+    return gulp
+        .src(input)
+        .pipe(sassdoc(sassdocOptions))
+        .resume();
+});
+
+gulp.task('watch', function() {
+    return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+        .watch(input, ['sass'])
+        // When there is a change,
+        // log a message in the console
+        .on('change', function(event) {
+            console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+        });
+});
+
+gulp.task('prod', ['sassdoc'], function () {
+    return gulp
+        .src(input)
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(output));
+});
+
+gulp.task('default', ['sass', 'watch']);
