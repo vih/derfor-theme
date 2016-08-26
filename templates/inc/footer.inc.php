cf35d56ae289c3648bada9f71644b2cc9e566090
Lars Olesen <lars@intraface.dk>
2016-08-26 11:22:01 +0000

    Fixed links

--- templates/inc/footer.inc.php
+++ templates/inc/footer.inc.php
@@ -4,11 +4,11 @@
     <p>
       Copyright &copy; <a href="http://vih.dk">Vejle Idrætshøjskole</a>
       <br/>
-      <a href="<?php echo url('node/7'); ?>">Ofte stillede spørgsmål</a>
+      <a href="<?php echo url('node/2'); ?>">Ofte stillede spørgsmål</a>
       <br/>
-      <a href="<?php echo url('node/36'); ?>">Om websitet</a>
+      <a href="<?php echo url('node/3'); ?>">Om websitet</a>
       <br/>
-      <a href="<?php echo url('node/29'); ?>">Disclaimer</a>
+      <a href="<?php echo url('node/4'); ?>">Disclaimer</a>
     </p>
   </footer>
 </div>
