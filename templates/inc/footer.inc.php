a239a786d67874edd33b5edb029b0a86729c0fbe
Lars Olesen <lars@intraface.dk>
2016-10-04 19:40:08 +0200

    Reworked footer so custom links can be added

--- templates/inc/footer.inc.php
+++ templates/inc/footer.inc.php
@@ -2,13 +2,7 @@
   <footer class="footer">
     <p class="pull-right"><a href="#">Til toppen</a></p>
     <p>
-      Copyright &copy; <a href="http://vih.dk">Vejle Idrætshøjskole</a>
-      <br/>
-      <a href="<?php echo url('node/2'); ?>">Ofte stillede spørgsmål</a>
-      <br/>
-      <a href="<?php echo url('node/3'); ?>">Om websitet</a>
-      <br/>
-      <a href="<?php echo url('node/4'); ?>">Disclaimer</a>
+      <?php print render($page['footer']); ?>
     </p>
   </footer>
 </div>
